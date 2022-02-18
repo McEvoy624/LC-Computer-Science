2e7285ab515e1b860b0fae468ee2f00335e79de4
McEvoy624 <95475217+McEvoy624@users.noreply.github.com>
2022-02-18 10:31:29 +0000

    Add files via upload

---
+++ ALT2/stats.py
@@ -0,0 +1,11 @@
+# Statistics module 18/02/22
+import statistics
+
+results = [98,95,58,85,82,78,]
+mean = statistics.mean(results)
+median = statistics.median(results)
+spread = statistics.stdev(results)
+
+print(mean)
+print(median)
+print(spread)
